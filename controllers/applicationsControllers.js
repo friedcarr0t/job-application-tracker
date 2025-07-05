6d783676ea14b835ecb5227012c89cae92a1225f
friedcarr0t <musalimrd@gmail.com>
2025-07-05 23:33:43 +0700

    adding route POST

--- controllers/applicationsControllers.js
+++ controllers/applicationsControllers.js
@@ -12,7 +12,27 @@ const getApp = (req, res) => {
 }
 
 const postApp = (req, res) => {
-    db.query('INSERT INTO applications')
+    const {nama_pt, posisi, tgl_apply, status, notes} = req.body
+    const allowedStatus = ["menunggu", "diterima", "ditolak"];
+    if(!nama_pt||!posisi||!status){
+        return res.status(400).json({error: "Semua field harus diisi"})
+    }
+    if(!allowedStatus.includes(status)){
+        return res.status(400).json({error: "Status tidak valid (menunggu, diterima, ditolak)"});
+    }
+
+    db.query('INSERT INTO applications (nama_pt, posisi, tgl_apply, status, notes) VALUES (?, ?, ?, ?, ?)',
+        [nama_pt, posisi, tgl_apply, status, notes], (err, results) => {
+            if(err){
+                console.error('Database error:', err);
+                return res.status(500).json({error: err.message});
+            }
+            res.status(201).json({message: "Data lamaran berhasil disimpan", id: results.insertId});
+        }
+    )
 }
 
-module.exports = {getApp};
+module.exports = {
+    getApp,
+    postApp
+};
--- routes/applicationsRoutes.js
+++ routes/applicationsRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {getApp} = require('../controllers/applicationsControllers');
+const applicationsControllers = require('../controllers/applicationsControllers');
 
-router.get('/', getApp);
+router.get('/app', applicationsControllers.getApp);
+router.post('/app', applicationsControllers.postApp);
 
 module.exports = router;    
