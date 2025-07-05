b0bb8a6b29a01b8de4369ed39bb6b42eab7692d7
friedcarr0t <musalimrd@gmail.com>
2025-07-05 16:19:02 +0700

    adding empty database

---
+++ applications.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 05 Jul 2025 pada 11.18
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jobapplications`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `applications`
+--
+
+CREATE TABLE `applications` (
+  `id` int(3) NOT NULL,
+  `nama_pt` varchar(50) NOT NULL,
+  `posisi` varchar(25) NOT NULL,
+  `tgl_apply` date NOT NULL,
+  `status` enum('menunggu','diterima','ditolak') NOT NULL,
+  `notes` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `applications`
+--
+ALTER TABLE `applications`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
